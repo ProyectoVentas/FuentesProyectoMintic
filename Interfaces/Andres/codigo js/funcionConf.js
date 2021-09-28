291fd2e4728aeadf878d5172ebddb67f63b76152
Andres_Coronado <andrescoronadob5@gmail.com>
2021-09-28 14:29:39 +1000

    Interfaz andres

---
+++ Interfaces/Andres/codigo js/funcionConf.js
@@ -0,0 +1,5 @@
+document.getElementById('confirmButton').addEventListener("click",referenciaConf);
+
+function referenciaConf() {
+   window.location.href = 'updateConf.html';
+}
---
+++ Interfaces/Andres/codigo js/funciones.js
@@ -0,0 +1,12 @@
+
+document.getElementById('searchButton').addEventListener("click",referencia);
+
+function referencia() {
+   window.location.href = 'mytable.html';
+}
+
+document.getElementById('update').addEventListener("click",referenciaUpdate);
+
+function referenciaUpdate() {
+   window.location.href = 'update.html';
+}
