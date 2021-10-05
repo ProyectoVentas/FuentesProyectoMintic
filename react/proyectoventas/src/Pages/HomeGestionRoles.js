733d3e0fd2067ceec9ab870104bf56ca5c7a31e3
Andres_Coronado <andrescoronadob5@gmail.com>
2021-10-05 15:35:56 +1100

    home y primera ventana Andres

---
+++ react/proyectoventas/src/Pages/HomeGestionRoles.js
@@ -0,0 +1,26 @@
+import React from "react";
+import NavbarSuperior from '../components/NavbarSuperior'
+import FooterPrincipal from '../components/FooterPrincipal'
+import BuscarRolInput from "../components/BuscarRolInput";
+import Buscar from "../components/ButtonBuscarEmpleadoRoles";
+
+
+class HomeGestionRoles extends React.Component{
+    render(){
+        return (
+            <div>
+            
+            
+<BuscarRolInput/>
+<Buscar/>
+
+
+           
+            
+            
+            
+            </div>
+        )
+    }
+}
+export default HomeGestionRoles
