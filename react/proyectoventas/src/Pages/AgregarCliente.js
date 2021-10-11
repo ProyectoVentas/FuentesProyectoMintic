14e9bccd7c6f1a03a3452afa3099bf13743798ac
Alejandra <alejandrafernandezg9701@gmail.com>
2021-10-11 18:02:31 -0500

    Intefaz agregarcliente React

---
+++ react/proyectoventas/src/Pages/AgregarCliente.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ClienteForm from "../components/ClienteForm";
+
+
+class AgregarCliente extends React.Component{
+
+    render(){
+        return(
+            <div>
+ 
+  <ClienteForm/>
+ 
+  
+  
+  
+  </div>
+        )
+        
+    }
+
+}
+export default AgregarCliente
